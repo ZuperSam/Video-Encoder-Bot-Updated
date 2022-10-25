f71604d60491aa378b00d948f7ad6f5d4d9bc79e
BLANK <114960961+KIRITO99-7@users.noreply.github.com>
2022-10-26 02:12:14 +0530

    Update ffmpeg.py

--- VideoEncoder/utils/ffmpeg.py
+++ VideoEncoder/utils/ffmpeg.py
@@ -187,7 +187,7 @@ async def encode(filepath, message, msg):
     # Metadata Watermark
     m = await db.get_metadata_w(message.from_user.id)
     if m:
-        metadata = '-metadata title=t.me/YUITOAKASH -metadata:s:v title=Weeb-Zone.Blogspot.com -metadata:s:a title=Weeb-Zone.Blogspot.com'
+        metadata = '-metadata title=t.me/YUITOAKASH -metadata:s:v title=t.me/YUITOAKASH -metadata:s:a title=t.me/YUITOAKASH'
     else:
         metadata = ''
 
